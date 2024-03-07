bed770dd9f1c46975fefc5dfae54e93c6445b3e8
mckoyd <dominick.mckoy@gmail.com>
2024-03-07 12:31:17 -0800

    adds hook for screen size | adds mobile bg for all mobile pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
+
 import PersonalInfo from "./PersonalInfo";
+import { ReactComponent as BgSidebarMobile } from "../assets/images/bg-sidebar-mobile.svg";
+import { useScreenSizeUpdate } from "../hooks/useScreenSizeUpdate";
+
+import "../styles/App.css";
+import { layout } from "../constants";
 
 const App: React.FC = () => {
+  const screenSize = useScreenSizeUpdate();
+
   return (
-    <section className="App">
+    <section className="app-section">
+      {screenSize <= layout.mobile && (
+        <BgSidebarMobile className="bg-sidebar-mobile" />
+      )}
       <Routes>
         <Route path="/" element={<PersonalInfo />} />
       </Routes>
