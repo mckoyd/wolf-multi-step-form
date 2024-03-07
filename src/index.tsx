537642b47c90ea9a826c95d347b784bc4129ac42
mckoyd <dominick.mckoy@gmail.com>
2024-03-07 08:26:33 -0800

    removes duplicate index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createHashRouter } from "react-router-dom";
-import App from "./App";
+import App from "./pages/App";
 
 import "./styles/index.css";
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
-import PersonalInfo from "./pages/PersonalInfo";
+import PersonalInfo from "./PersonalInfo";
 
 const App: React.FC = () => {
   return (
