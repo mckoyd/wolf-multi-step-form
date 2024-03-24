95da10c67adc9c708ed1638631170bf8d8249778
mckoyd <dominick.mckoy@gmail.com>
2024-03-23 23:23:44 -0700

    Updates

--- src/state/addOnState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const addOnState = atom({
-  key: "addOnState",
-  default: [] as { title: string; price: number }[],
-});
--- src/state/planOptionData.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-import { IPlanOption } from "../constants";
-
-export const selecetedPlanOptionState = atom({
-  key: "selectedPlanOptionState",
-  default: {} as { price: number; title: string; isYearly: boolean },
-});
